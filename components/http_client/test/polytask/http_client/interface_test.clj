f16799a12728427a29851928ab823b4f166fd213
Andrew Leverette <andrewleverette@gmail.com>
2025-02-07 13:24:31 -0600

    HTTP Client Component (#1)
    * Add HTTP client
    
    * Update error returns for task api
    
    * Add HTTP client component that can be used on server or in browser

---
+++ components/http_client/test/polytask/http_client/interface_test.clj
@@ -0,0 +1,6 @@
+(ns polytask.http_client.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [polytask.http_client.interface :as http_client]))
+
+(deftest dummy-test
+  (is (= 1 1)))
--- development/src/development/core.clj
+++ development/src/development/core.clj
@@ -1,7 +1,8 @@
 (ns development.core
   (:require
    [integrant.core :as ig]
-   [polytask.config.interface :as c]))
+   [polytask.config.interface :as c]
+   [polytask.http-client.interface :as http-client]))
 
 (comment
   (def config (c/get-config :polytask.api/system))
@@ -9,3 +10,23 @@
   (def system (ig/init config))
   system
   (ig/halt! system))
+
+(http-client/request
+ {:method :get
+  :url "http://localhost:8000/tasks"})
+
+(http-client/request
+ {:method :get
+  :url "http://localhost:8000/tasks/5f811e2e-05cc-4cf6-bd55-adf51e5090a3"})
+
+(http-client/request
+ {:method :put
+  :url "http://localhost:8000/tasks/5f811e2e-05cc-4cf6-bd55-adf51e5090a3"
+  :body {:status "in-progress"}})
+
+(http-client/request
+ {:method :post
+  :url "http://localhost:8000/tasks"
+  :body {:title "Study Clojure for 1 hour"
+         :description "Using the curring current learn clojure goals and milestones, study Clojure for 1 hour."
+         :status "not-started"}})
