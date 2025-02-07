44ffb8288a14d8f359cf0f1ed831295d89736c75
Andrew Leverette <andrewleverette@gmail.com>
2025-02-07 16:33:00 -0600

    Update build process and add skeleton CLI base

---
+++ bases/cli/src/polytask/cli/core.clj
@@ -0,0 +1,5 @@
+(ns polytask.cli.core)
+
+(defn -main
+  [& args]
+  (println "Hello, World!"))
---
+++ bases/cli/test/polytask/cli/core_test.clj
@@ -0,0 +1,6 @@
+(ns polytask.cli.core-test
+  (:require [clojure.test :as test :refer :all]
+            [polytask.cli.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
